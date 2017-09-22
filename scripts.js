d3f1343a344951b1d6a5fd2337e8e2ea29f13d9e
katapo <kata.pochec@gmail.com>
2017-09-22 14:57:59 +0200

    else line changed

--- scripts.js
+++ scripts.js
@@ -1,18 +1,17 @@
 var a = -5;
 var b = 5;
-var value = (a * a) + (2 * a * b) - (b * b)
+var value = (a * a) + (2 * a * b) - (b * b);
 console.log(value);
 
-if ( value > 0 ) {
-  console.log('wynik dodatni')
+if (value > 0) {
+  console.log('wynik dodatni');
 }
 
-if ( value < 0 ) {
-  console.log('wynik ujemny')
+else if (value < 0) {
+  console.log('wynik ujemny');
 }
 
-if (value == 0 ) {
-  console.log('wynik rowny zero')
+else {
+  console.log('wynik rowny zero');
 }
-
 
